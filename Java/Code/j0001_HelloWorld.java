6ad64e967e5c448944d25a9a2b9332453a4f69a8
Mohd Shakir Siddiqui <146057248+Shakir258@users.noreply.github.com>
2024-11-05 22:56:04 +0530

    Add files via upload

---
+++ Java/Code/j0001_HelloWorld.java
@@ -0,0 +1,7 @@
+package Java.Code;
+
+public class j0001_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Java/Practice/hellow.java
@@ -0,0 +1,7 @@
+package Java.Practice;
+
+public class hellow {
+    public static void main(String[] args) {
+        System.out.println("hellow");
+    }
+}
