1141bf6382e32cb30cb9cd3624888cc558f4ef17
Mohd Shakir Siddiqui <146057248+Shakir258@users.noreply.github.com>
2024-12-07 13:51:53 +0530

    Add files via upload

---
+++ Java/Code/D0003_BasiceJava.java
@@ -0,0 +1,22 @@
+package Java_code.ApnaCollege.DayCode;
+
+public class D0003_BasiceJava {
+    
+    public static void main(String[] args){
+        // System.out.println("Hellow Wold");
+        // System.out.print("Hellow Wold");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+
+        int a = 10 ;
+        int b = 5 ;
+        System.out.println(a+b);
+        System.out.println(a);
+        System.out.println(b);
+        
+    }
+}
+
+
