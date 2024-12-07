413fa1ae0640573f37c8d3a8b64555867ec80526
Mohd Shakir Siddiqui <146057248+Shakir258@users.noreply.github.com>
2024-12-07 13:55:41 +0530

    Add files via upload

---
+++ Java/Code/j001_SumOfBinary.java
@@ -0,0 +1,15 @@
+package Java_code.Web_exercise;
+
+import java.util.Scanner;
+
+public class j001_SumOfBinary {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first Number : ");
+        long binary1= sc.nextLong();
+        System.out.print("Enter the Second Number : ");
+        long binary2= sc.nextLong();
+        
+        int reminder = 0 , i=0;
+    }
+}
