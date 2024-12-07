f8513c2b4b825aa3e4bf948652c0abf1e77a20e3
Mohd Shakir Siddiqui <146057248+Shakir258@users.noreply.github.com>
2024-12-07 13:56:15 +0530

    Add files via upload

---
+++ Java/Code/j002_constructor_1.java
@@ -0,0 +1,27 @@
+
+//Write a Java program to create a class called "Cat" with instance variables name and age.
+// Implement a default constructor that initializes the name to "Unknown" and the age to 0.
+// Print the values of the variables.
+
+
+package Java_code.Web_exercise;
+
+class Cat{
+    String name ;
+    int age ;
+    Cat(){
+        name = "Unknown";
+        age = 0;
+//        System.out.println(name);
+//        System.out.println(age);
+
+    }
+}
+
+public class j002_constructor_1 {
+    public static void main(String[] args) {
+        Cat obj = new Cat();
+        System.out.println(obj.name);
+        System.out.println(obj.age);
+    }
+}
