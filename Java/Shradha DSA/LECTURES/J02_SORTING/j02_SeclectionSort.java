a2af597ed23371e832c3863abd52d4bc86671fb0
Mohd Shakir Siddiqui <146057248+Shakir258@users.noreply.github.com>
2024-12-11 12:52:33 +0530

    Update j02_SeclectionSort.java

--- Java/Shradha DSA/LECTURES/J02_SORTING/j02_SeclectionSort.java
+++ Java/Shradha DSA/LECTURES/J02_SORTING/j02_SeclectionSort.java
@@ -18,7 +18,7 @@ public class j02_SeclectionSort {
         }
     }
     public static void main(String[] args) {
-        int arr[] = {5,60,30,8,11,8,9,233,96,78,4,23,42};
+        int arr[] = {5,60,30,8,11,8,9,233,96,78,4,23,42};        //Array declair
 
         seclectionSort(arr);
         for(int i=0;i<arr.length;i++){
