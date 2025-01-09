377ee6dbfe5556042ca9e835680fb7a32b76606c
Mohd Shakir Siddiqui <146057248+Shakir258@users.noreply.github.com>
2025-01-09 11:34:23 +0530

    Update basic.c

--- C/code/ARRAY/basic.c
+++ C/code/ARRAY/basic.c
@@ -3,7 +3,7 @@ int main(){
 //     int arr[5] ={6,7,4,0,7};
 // arr[3]=100;
 //     printf("%d = valiue of index  ",arr[3]);
-printf("shakirsiddiqui\n");
+printf("shakir siddiqui\n");
 char arr[4] ={'a','b','a',};
 printf("value is = %c  \n",arr[2]);
     return 0;
