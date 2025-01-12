ed33b8774bea01274b6ad2532e1b7be46b19779e
shakir258 <malik844594@gmail.com>
2025-01-12 20:16:02 +0530

    string method

--- Python/Code/p002_string.py
+++ Python/Code/p002_string.py
@@ -7,5 +7,22 @@ gfjkdfjgjd
 print("My name is ", name)
 print("My name is ", name2)
 print(name[0]) 
+
+
 for character in name2:
     print(character)
+
+
+#string slicing 
+
+name = "shakir"
+print(name[0:4]) #n-1 including 0 not 4
+print(name[0:-2]) #n-1
+print(name[-4:-2]) #n-1
+
+nm = "Harry"
+print(nm[-4:-2])
+ 
+# length of string
+name = "shakir"
+print(len(name))
