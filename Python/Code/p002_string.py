09ae74e8086d04c199ef9bb810c015120a0bbf2c
shakir258 <malik844594@gmail.com>
2025-01-12 19:37:06 +0530

    updated

--- Python/Code/p002_string.py
+++ Python/Code/p002_string.py
@@ -1,2 +1,11 @@
 name = "shakir"
+name2 = """shakir  
+vjjlfdjgjd
+jgfjsgljsd
+gfjkdfjgjd
+"""
 print("My name is ", name)
+print("My name is ", name2)
+print(name[0]) 
+for character in name2:
+    print(character)
