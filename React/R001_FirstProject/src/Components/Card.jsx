525262618310b19ba2587f219e4ac2f345a02976
shakir258 <malik844594@gmail.com>
2025-01-25 22:29:12 +0530

    new functun

--- React/R001_FirstProject/src/Components/Card.jsx
+++ React/R001_FirstProject/src/Components/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 function Card(){
     return (
         <div>
-            <h1 className="text-red-500">shakir</h1>
+            <h1 className="shakir">shakir</h1>
         </div>
     )
 }
