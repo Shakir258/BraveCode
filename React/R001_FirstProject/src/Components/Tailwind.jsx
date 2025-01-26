1a875979fc9c9becc93562a1dcbbec4a05eb36a9
shakir258 <malik844594@gmail.com>
2025-01-26 21:49:11 +0530

    update

--- React/R001_FirstProject/src/Components/Tailwind.jsx
+++ React/R001_FirstProject/src/Components/Tailwind.jsx
@@ -1,9 +1,10 @@
 import react from "react";
+import pr from '../assets/pf.png';
 
 
 function Tailwind() {
     return (
-        <div className="main w-full h-screen bg-white font-['gilroy']">
+        <div className="main w-full h-screen bg-white font-['gilroy light']">
             <div className="container mx-auto ">
                 <div className="navbar flex justify-between items-center w-full py-6 px-4">
                     <h3 className="font-semibold">neganou.Design</h3>
@@ -16,7 +17,17 @@ function Tailwind() {
                     <i class="ri-menu-line lg:hidden"></i>
                     
                 </div>
-            </div>
+
+                <div className="hero w-full flex flex-col justify-between items-center">
+                    <div className="hero-1 w-24 h-24 bg-blue-400 rounded-full mt-20">
+                        <img src={pr} alt="image" />
+                    </div>
+                    <h3 className="text-xl font-semibold mt-10">I'm Shakir siddiqui</h3>
+                    <h1 className=" w-3/4  tracking-tight text-4xl leading-none font-semibold text-center mt-3">Build Digital Product, brand, and exprimce</h1>
+                    <p className="w-3/4 text-center mt-4 font-semibold trancking-tight text-sm">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam, dolor quaerat voluptas magnam maiores recusandae.</p>
+                    <button className="bg-black text-white py-2 px-4 rounded-full mt-5 font-semibold text-sm">Connect with me</button>
+                </div>
+            </div> 
         </div>
     )
 }
--- React/R001_FirstProject/src/main.jsx
+++ React/R001_FirstProject/src/main.jsx
@@ -2,11 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Card from './Components/Card.jsx'
-import Tailwind from './Components/Tailwind.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Tailwind />
+    <App />
   </StrictMode>,
 )
