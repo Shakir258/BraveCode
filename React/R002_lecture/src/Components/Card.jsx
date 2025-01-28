d9e3aae24a7429f73818046fec7789f9e247634c
shakir258 <malik844594@gmail.com>
2025-01-28 20:39:36 +0530

    update

--- React/R002_lecture/src/Components/Card.jsx
+++ React/R002_lecture/src/Components/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Card(){
 
-    var data = [{image : "https://images.unsplash.com/photo-1736230990003-a98eea26ea1f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0fHx8ZW58MHx8fHx8" , name: "samsung" , description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, veritatis.", instock: false},
+    var data = [{image : "https://images.unsplash.com/photo-1736230990003-a98eea26ea1f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw0fHx8ZW58MHx8fHx8" , name: "samsung" , description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, veritatis.", instock: false}, 
         {image : "https://images.unsplash.com/photo-1604212666403-8af98b2081e4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzR8fGlwaG9uZXxlbnwwfHwwfHx8MA%3D%3D" , name: "apple" , description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, veritatis.",instock: false },
         {image : "https://images.unsplash.com/photo-1611648694964-a4cbd6ab1c70?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" , name: "laptop" , description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, veritatis.",instock: true},
     ]
