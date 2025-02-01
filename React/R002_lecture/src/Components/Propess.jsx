943be2d9649b6dd843a6d7ddf2bf10c6248d2e31
shakir258 <malik844594@gmail.com>
2025-02-01 21:56:47 +0530

    use state in propes

--- React/R002_lecture/src/Components/Propess.jsx
+++ React/R002_lecture/src/Components/Propess.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
 
-function Propess({change,color}){
+function Propess({value ,dummy}){
+    const {name , image , profession , friend} = value;
     return (
-        <div>
-            <button className={`${color} text-white py-2 px-4 m-10 rounded-full`} >{change}</button>
+        <div className='w-52  bg-white rounded-md overflow-hidden'>
+            <div className='w-full h-32 bg-sky-300'>
+                <img className='w-full h-full object-cover' src={image} alt="" />
+            </div>
+            <div className="w-full p-3">
+                <h3 className='text-xl font-semibold '>{name}</h3>
+                <h5 className='text-xm'>{profession}</h5>
+                <button onClick={dummy} className=' mt-4 px-3 py-1 text-xm text-white bg-blue-400 font-semibold rounded-md '> { friend === true ? "Unfollow" : "Follow"}</button>
+            </div>
         </div>
     )
 }
