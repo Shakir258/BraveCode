274e3b3496e524f2b82ee28fdc82efdc1a799fb3
shakir258 <malik844594@gmail.com>
2025-02-12 22:34:19 +0530

    change value

--- React/R005_Project1/src/Components/Loding.jsx
+++ React/R005_Project1/src/Components/Loding.jsx
@@ -2,9 +2,19 @@ import React from 'react';
 
 function Loding(props) {
     return (
-        <div className="flex justify-center items-center w-full h-screen">
-            <h1 className="text-6xl font-semibold">loading...</h1>
-        </div>
+        <div className="w-[70%] mx-auto p-[10%] flex gap-5 animate-pulse">
+      {/* Image Placeholder */}
+      <div className="w-[40%] h-[300px] bg-gray-300 rounded-md"></div>
+
+      {/* Text Content Placeholder */}
+      <div className="w-[60%]">
+        <div className="h-6 bg-gray-300 rounded-md mb-4 w-3/4"></div>
+        <div className="h-4 bg-gray-300 rounded-md mb-3 w-1/2"></div>
+        <div className="h-5 bg-gray-300 rounded-md mb-3 w-1/4"></div>
+        <div className="h-20 bg-gray-300 rounded-md mb-3 w-full"></div>
+        <div className="h-20 bg-gray-300 rounded-md w-32"></div>
+      </div>
+    </div>
     );
 }
 
